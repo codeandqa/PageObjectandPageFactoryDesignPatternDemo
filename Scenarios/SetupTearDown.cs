1dc1296f5b497dc443d4c4f3e37f371e9a4cad4f
Aditya <shahi.aditya@gmail.com>
2014-05-10 15:40:19 -0400

    First start to end exeuction

---
+++ Scenarios/SetupTearDown.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+namespace Scenario_ShoppingCart.Scenarios
+{
+   [TestClass()]
+    public partial class Scenarios_Demo
+    {
+
+        private IWebDriver driver;
+        
+        private string baseURL;
+        [TestInitialize]
+        public void SetupTest()
+        {
+            string extension = @"C:\Users\Aditya\Documents\Visual Studio 2013\Projects\Scenario_ShoppingCart\packages\firebug.xpi";
+            FirefoxProfile ffProfile = new FirefoxProfile();
+            ffProfile.AddExtension(extension);
+            driver = new FirefoxDriver(ffProfile);
+            baseURL = "http://demo.virtuemart.net";
+        }
+        [TestCleanup]
+        public void CleanUpTest()
+        {
+            driver.Close();
+            driver.Dispose();
+        }
+
+
+
+
+       [Description("This property specifies the active Widget")]
+        public void WaitForVisibleElement(string locator)
+        {
+            //Wait for Shopping cart Page. Verify Items. and click on checkout button.
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until<IWebElement>(ExpectedConditions.ElementIsVisible(By.CssSelector(locator)));
+        }
+
+    }
+}
---
+++ Scenarios/VerifyShoppingCartbehavior.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace Scenario_ShoppingCart.Scenarios
+{
+    public partial class Scenarios_Demo
+    {
+        [TestMethod]
+        public void VerifyShoppingCartBehavior1()
+        {
+            //Open URL
+            driver.Navigate().GoToUrl(baseURL + "/");
+            //Click on Computer Tab.
+            driver.FindElement(By.CssSelector("div[id='product-presenter']>ul[id='category-names']>li:nth-child(2)")).Click();
+            System.Threading.Thread.Sleep(2000);
+
+            //Get the first row from the computer Item grid.
+            IWebElement rows = driver.FindElement(By.CssSelector("div[id='product-container'][style='display: block;']")); 
+            
+            //click on add to cart button.
+            rows.FindElement(By.CssSelector("input[class='btn btn-success addtocart-button']")).Click();
+            
+            //wait for alert screen and click on Show Cart link.
+            System.Threading.Thread.Sleep(2000);
+            IWebElement alert = driver.FindElement(By.Id("facebox"));
+            alert.FindElement(By.LinkText("Show Cart")).Click();
+            
+            //Wait for Shopping Cart.Click on Checkout now button.
+            WaitForVisibleElement("div[id='rt-mainbody-custom']");
+            string qantity = driver.FindElement(By.CssSelector("input[class='quantity-input js-recalculate']")).GetAttribute("value");
+            Assert.AreEqual("1", qantity);
+            driver.FindElement(By.Name("checkout")).Click();
+
+            //wait for your Account Detail Page. Enter Demo demo and click login. return as 
+            WaitForVisibleElement("div[class='vm-login-form']");
+            driver.FindElement(By.Name("username")).SendKeys("demo");
+            driver.FindElement(By.Name("password")).SendKeys("demo" + Keys.Enter);
+            //wait for login greeting text that you are logged in.
+            WaitForVisibleElement("div[class='login-greeting']");
+            //Verify the Hi demo text as greeting. 
+            Assert.AreEqual("Hi demo,", driver.FindElement(By.CssSelector("div[class='login-greeting']")).Text);
+            
+            //VerifyAccount detail Page. CLick on Shopping Cart Link on the page. Change the quanitity 
+            driver.FindElement(By.LinkText("Shopping Cart")).Click();
+            
+            //Verify Shopping Cart Page. with Item.
+            Assert.AreEqual("Shopping cart", driver.Title);
+            WaitForVisibleElement("div[id='rt-mainbody-custom']");
+            WaitForVisibleElement("div[class='price-type']");
+
+            //Clear the cart. 
+            driver.FindElement(By.CssSelector("input[class='quantity-input js-recalculate']")).Clear();
+            driver.FindElement(By.CssSelector("input[class='quantity-input js-recalculate']")).SendKeys("0");
+            driver.FindElement(By.CssSelector("input[class='refresh-icon']")).Click();
+            
+            //Verify empty Shopping Cart.
+            WaitForVisibleElement("div[class='message']");
+            Assert.AreEqual("There are no products in your cart.", driver.FindElement(By.CssSelector("div[class='message']")).Text);
+            
+            //logout.
+            driver.FindElement(By.CssSelector("input[value='Log out']")).Click();
+            //Wait for login/password input field appeared after click.
+            WaitForVisibleElement("input[id='modlgn-username']");
+
+        }
+    }
+}
